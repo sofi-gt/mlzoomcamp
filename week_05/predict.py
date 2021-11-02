769716d67318423d40a46af3b67fb48f3df7ae16
Sofi GS <19873735+sofi-gt@users.noreply.github.com>
2021-11-01 18:29:21 -0600

    deploying model with flask

--- week_05/predict.py
+++ week_05/predict.py
@@ -1,33 +1,34 @@
 import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
+
 model_file = 'model_C=1.0.bin'
 
 with open(model_file, 'rb') as f_in: 
     (dv, model) = pickle.load(f_in)
 
-customer = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
+app = Flask('churn')
 
-X = dv.transform([customer])
-y_pred = model.predict_proba(X)[0, 1]
+@app.route('/predict', methods=['POST']) # post because we need to send info about customer and it is not easy using GET method
+def predict():
+    customer = request.get_json()
+    # separate the core logic in another function
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
 
-print('input', customer)
-print(f'churn probability: {y_pred}')
+    result = {
+        'churn_probability': float(y_pred),
+        'churn': bool(churn) # turns numpy boolean into python boolean to avoid error 500
+    }
+    return jsonify(result)
+
+if __name__ == "__main__": # python main method
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
+# request will be sent using JSON file format.
+# like python dictionary but with double quotes
+# 404 not found
+# 405 method no allowed
+# cannot send a POST request from browser easily
